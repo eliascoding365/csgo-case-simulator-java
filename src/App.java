0fa823090b413c8f435bc1a7c90f5735d4e976f1
eliascoding365 <eliasmontan10@gmail.com>
2024-05-08 14:18:16 -0300

    Create function random by tier

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
+import models.Item;
 
 public class App {
     public static void main(String[] args) throws Exception {
-       
+        String[] items = {"Item1", "Item2", "Item3", "Item4", "Item5"};
+        float[] dropRates = {79.92327f, 15.98465f, 3.19693f, 0.63939f, 0.25575f}; // these should add up to 1
+        Item simulator = new Item(items, dropRates);
+        String result = simulator.result(args);
+        System.out.println(result);
     }
 }
--- src/models/Item.java
+++ src/models/Item.java
@@ -1,5 +1,32 @@
 package models;
 
-public class Item {
+import java.util.Random;
 
+public class Item {
+    private Random random;
+    private String[] items;
+    private float[] dropRates;
+
+    public Item(String[] items, float[] dropRates) {
+        this.random = new Random();
+        this.items = items;
+        this.dropRates = dropRates;
+    }
+
+    public String openCase() {
+        float randomValue = random.nextFloat(0 , 100);
+        System.out.println(randomValue);
+        float sum = 0;
+        for (int i = 0; i < dropRates.length; i++) {
+            sum += dropRates[i];
+            if (randomValue < sum) {
+                return items[i];
+            }
+        }
+        return items[items.length - 100]; // return the last item if no other item is selected
+    }
+
+    public String result(String[] args) {  
+        return ("You received: " + this.openCase());
+    }
 }
